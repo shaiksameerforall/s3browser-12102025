7e8c9c1918a2752d92142df365644e422f2e6346
shaik sameer <shaiksameerforall@gmail.com>
2025-12-10 11:49:53 +0300

    adding storage backend

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,11 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource.js';
 import { data } from './data/resource.js';
+import { storage } from './storage/resource';
+
 
 defineBackend({
   auth,
   data,
+  storage
 });
--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
 import { Authenticator } from '@aws-amplify/ui-react';
+import { StorageBrowser } from '../components/StorageBrowser';
 
 Amplify.configure(outputs);
 
@@ -38,6 +39,11 @@ export default function App() {
         <main>
           <h1>Hello {user?.username}</h1>
           <button onClick={signOut}>Sign out</button>
+
+          {/* StorageBrowser Component */}
+          <h2>Your Files</h2>
+          <StorageBrowser /> 
+          
         </main>
       )}
     </Authenticator>
